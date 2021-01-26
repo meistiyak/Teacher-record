69d9b79fc6607c42add7d719d5adf22028f6b1ab
istiyak <meistiyak@hotmail.com>
2021-01-26 09:32:16 +0600

    Fix delete view in url

--- teachers/urls.py
+++ teachers/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
   path('<int:pk>/', views.TeacherDetailView.as_view(), name='teacher-detail'),
   path('create/', views.TeacherCreateView.as_view(), name='teacher-create'),
   path('<int:pk>/update/', views.TeacherUpdateView.as_view(), name='teacher-update'),
-  path('delete/<int:pk>/', views.TeacherDetailView.as_view(), name='teacher-delete'),
+  path('delete/<int:pk>/', views.TeacherDeleteView.as_view(), name='teacher-delete'),
   path('upload/', views.TeacherBulkUploadView.as_view(), name='teacher-upload'),
   path('download-csv/', views.download_csv, name='download-csv'),
 
